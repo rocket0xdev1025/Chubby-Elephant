8e2de99404ed5437e8dd460d4b8255d9a6b75013
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-05-27 03:41:41 +0900

    update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DICKEY_CONFIG = {
-  contract: "0xcomignsoon",
+  contract: "0xe665ff86a98a52a1eb64fda86e77ded61d2d58e2",
   twitterUrl: "https://x.com/ChubbyElephantX",
   telegramUrl: "https://t.me/ChubbyElephant",
   adminPassword: "2323",
