54fce6ab9aa3b48c29405e9b4924a3b27176effe
rocket0xdev1025 <rocket0xdev@gmail.com>
2026-05-27 03:34:41 +0900

    update

--- assets/index-Bl5ygz9v.js
+++ assets/index-Bl5ygz9v.js
@@ -4225,7 +4225,7 @@ const Ql = ["id"],
   }),
   uc = gt(ac, [["__scopeId", "data-v-8e4bc7fd"]]),
   dt = {
-    contract: "0xcomingsoon",
+    contract: "0xe665ff86a98a52a1eb64fda86e77ded61d2d58e2",
     twitterUrl: "https://x.com/ChubbyElephantX",
     telegramUrl: "https://t.me/ChubbyElephant",
     adminPassword: "2323",
